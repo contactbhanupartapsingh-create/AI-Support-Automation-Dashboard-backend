d2d41238326940077aa239b46ff57ec3ea2dda5a
cloand <bhanupartap789@gmail.com>
2026-03-06 13:15:40 +0530

    Fix: api routes, added : multiple status filters for all routes

--- src/dto/ticketChangeStatus.dto.ts
+++ src/dto/ticketChangeStatus.dto.ts
@@ -2,10 +2,6 @@ import { IsEnum, IsNotEmpty, IsNumber} from "class-validator";
 import { TicketStatus } from "src/static";
 
 export class TicketChangeStatusDto {
-    @IsNumber()
-    @IsNotEmpty()
-    id: number;
-  
     @IsNotEmpty()
     @IsEnum(TicketStatus)
     status: TicketStatus;
--- src/dto/ticketRestore.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsNumber} from "class-validator";
-
-export class TicketRestoreDto {
-    @IsNumber()
-    @IsNotEmpty()
-    id: number;
-}
-    
